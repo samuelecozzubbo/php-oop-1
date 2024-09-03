1427bd8da634e4870886c231fb52af68d1983063
samuelecozzubbo <samuele.cozzubbo@gmail.com>
2024-09-03 16:22:42 +0200

    Add style

--- index.php
+++ index.php
@@ -8,15 +8,16 @@ require_once __DIR__ . '/data/db.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap-grid.css' integrity='sha512-hhSu9overYjKfSjPCtJW3688VHkfBh+W1pR5Mysll91bOJwGjYntytGTtVXb2aisFOaYXXDrO38NKXDRPJWu7A==' crossorigin='anonymous' />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>php-oop-1</title>
 </head>
 
 <body>
     <div class="container my-5">
         <div class="row">
+            <h1 class="text-bg-success p-3">Movie List</h1>
             <?php foreach ($db as $movie): ?>
-                <div class="card col-4">
+                <div class="card col-3 text-bg-light p-3">
                     <div class="card-body">
                         <h5 class="card-title"><?php echo $movie->getMovieTitle() ?></h5>
                         <h6 class="card-subtitle mb-2 text-body-secondary"><?php echo $movie->getMovieInfo() ?></h6>
