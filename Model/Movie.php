9a278d55809181db2d38afc9b84d04172093088d
samuelecozzubbo <samuele.cozzubbo@gmail.com>
2024-09-03 15:05:30 +0200

    Build: add constructor and fix properties

--- Model/Movie.php
+++ Model/Movie.php
@@ -1,7 +1,19 @@
 <?php
 class Movie
 {
-    public $name;
+    //propietà obbligatorie
+    public $title;
     public $duration;
     public $release_year;
+    //propietà non obbligatorie
+    public $box_office;
+    public $genre = [];
+
+    //COSTRUTTORE
+    function __construct($_title, $_duration, $_release_year)
+    {
+        $this->title = $_title;
+        $this->duration = $_duration;
+        $this->release_year = $_release_year;
+    }
 }
